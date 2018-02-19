3fa06dd57a5eb23a5e4c50c3387c15c39a8fb9eb
Eldwin Xu <eldwinx@sfu.ca>
2018-02-18 17:04:45 -0800

    Finished the website [outside of purchase process]
    Finished: Blog page, company page, blog detail page, contact page, game detail page
    
    Fixed minor bugs
    
    Linked all pages

--- Website/index.js
+++ Website/index.js
@@ -8,3 +8,11 @@ $(document).on("scroll", function() {
 	}
 
 });
+
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+};
+var map=new google.maps.Map(document.getElementById("contactMap"),mapProp);
+}
