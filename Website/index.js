1a51825e968eb207ca989ffe9e2378473f3dd6ca
Eldwin Xu <eldwinx@sfu.ca>
2018-02-24 15:25:16 -0800

    Content update

--- Website/index.js
+++ Website/index.js
@@ -11,7 +11,7 @@ $(document).on("scroll", function() {
 
 function myMap() {
 var mapProp= {
-    center:new google.maps.LatLng(51.508742,-0.120850),
+    center:new google.maps.LatLng(49.2845957,-123.1127161,279),
     zoom:5,
 };
 var map=new google.maps.Map(document.getElementById("contactMap"),mapProp);
